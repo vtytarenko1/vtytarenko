94afc6cfb722e257784b4aba383b939aa64b4400
Viacheslav.Tytarenko <Viacheslav.Tytarenko@gamesysgroup.com>
2023-06-06 11:45:27 +0300

    HomeWork Lesson 2

---
+++ src/main/java/home_work3/Rectangle.java
@@ -0,0 +1,39 @@
+package home_work3;
+
+public class Rectangle extends Figure {
+    private static final String NAME = "Rectangle";
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
