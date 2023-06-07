450d0d3464b7cdb53ed0a3fe7c2a8bffddfea91f
vtytarenko1 <132065312+vtytarenko1@users.noreply.github.com>
2023-06-07 15:28:58 +0300

    Revert "HomeWork Lesson 4"

--- src/main/java/HomeWork_4/ArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package HomeWork_4;
-
-public class ArrayDataException extends IllegalArgumentException {
-    public ArrayDataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/HomeWork_4/ArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package HomeWork_4;
-
-public class ArraySizeException extends IllegalArgumentException {
-    public ArraySizeException(String message) {
-        super(message);
-    }
-}
