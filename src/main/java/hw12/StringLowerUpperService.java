0d8f5494ce83a9f7489647abda724383430edbf4
Viacheslav.Tytarenko <Viacheslav.Tytarenko@gamesysgroup.com>
2023-11-06 11:02:32 +0200

    HomeWork Lesson 12

---
+++ src/main/java/hw12/StringLowerUpperService.java
@@ -0,0 +1,14 @@
+package hw12;
+/**
+ * @author spasko
+ */
+public class StringLowerUpperService {
+
+    public String toUpper(String input) {
+        return input.toUpperCase()+"!";
+    }
+
+    public String toLower(String input) {
+        return input.toLowerCase();
+    }
+}
